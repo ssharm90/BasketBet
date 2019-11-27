e1b948367cceee9a96affe0c94dd7780230e6845
shreyasharma <ssharm90@illinois.edu>
2019-11-26 22:31:57 -0600

    Finished UI for main page, added Logcat functionality with button.

--- app/src/main/java/com/example/basketbet/MainActivity.java
+++ app/src/main/java/com/example/basketbet/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.basketbet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static android.view.View.*;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button begin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        begin = findViewById(R.id.begin);
+
+        begin.setOnClickListener(unused -> {
+            Log.i("button", "begin button was clicked!");
+        });
+
     }
+
+
 }
